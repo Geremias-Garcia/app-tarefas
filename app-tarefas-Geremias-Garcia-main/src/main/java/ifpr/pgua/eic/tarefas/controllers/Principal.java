96629bec927f6ee62b41b3b204ad061ed63cde30
Geremias Garcia <88598645+Geremias-Garcia@users.noreply.github.com>
2023-09-23 01:29:52 -0300

    Add files via upload
    primeiros passos projeto

---
+++ app-tarefas-Geremias-Garcia-main/src/main/java/ifpr/pgua/eic/tarefas/controllers/Principal.java
@@ -0,0 +1,23 @@
+package ifpr.pgua.eic.tarefas.controllers;
+
+import ifpr.pgua.eic.tarefas.App;
+import javafx.fxml.FXML;
+
+public class Principal {
+    
+   
+}
+
+
+/*
+ *
+public class Principal {
+    
+
+    @FXML
+    private void cadastrarGenero(){
+        App.pushScreen("CADASTROGENERO");
+    }
+
+    
+ */
---
+++ app-tarefas-Geremias-Garcia-main/src/main/java/ifpr/pgua/eic/tarefas/model/entities/Tarefa.java
@@ -0,0 +1,5 @@
+package ifpr.pgua.eic.tarefas.model.entities;
+
+public class Tarefa {
+
+}
